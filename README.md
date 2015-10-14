43cdf4b74c820f679f4d0028bab3c971ab33e6f7
terryshek <terryshek@msn.com>
2015-10-14 17:20:10 +0800

    update regularly

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 first commission
+https://medium.com/materialup-exploring-material-design/getting-started-with-angular-material-3ee3cdb8ecd8
--- app/index.js
+++ app/index.js
@@ -1,5 +1,22 @@
 var app = angular.module('StarterApp', ['ngMaterial']);
 
+app.config(function($mdThemingProvider) {
+    var customBlueMap = 		$mdThemingProvider.extendPalette('light-blue', {
+        'contrastDefaultColor': 'light',
+        'contrastDarkColors': ['50'],
+        '50': 'ffffff'
+    });
+    $mdThemingProvider.definePalette('customBlue', customBlueMap);
+    $mdThemingProvider.theme('default')
+        .primaryPalette('customBlue', {
+            'default': '600',
+            'hue-1': '50'
+        })
+        .accentPalette('pink');
+    $mdThemingProvider.theme('input', 'default')
+        .primaryPalette('grey')
+});
+
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
     $scope.toggleSidenav = function(menuId) {
         $mdSidenav(menuId).toggle();
